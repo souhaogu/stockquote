3b57d759056947eab4a6a1db5d905d68a8b6f118
fly1232 <491876014@qq.com>
2018-08-13 00:03:17 +0800

    功能修改：    将welcome.jsp中文本修改为stock_quote    datasource改为自定义的BasicDataSourceExt，以防止内存失败

---
+++ stockquote_dao/src/main/java/com/soustock/stockquote/common/BasicDataSourceExt.java
@@ -0,0 +1,34 @@
+package com.soustock.stockquote.common;
+
+import org.apache.commons.dbcp.BasicDataSource;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
+
+/**
+ * Created by xuyufei on 2018/8/12.
+ */
+public class BasicDataSourceExt extends BasicDataSource {
+
+    @Override
+    public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+        return null;
+    }
+
+    @Override
+    public synchronized void close() throws SQLException {
+        DriverManager.deregisterDriver(DriverManager.getDriver(url));
+        super.close();
+    }
+
+    @Override
+    public <T> T unwrap(Class<T> iface) throws SQLException {
+        return null;
+    }
+    @Override
+    public boolean isWrapperFor(Class<?> iface) throws SQLException {
+        return false;
+    }
+}
--- stockquote_interface/src/main/webapp/welcome.jsp
+++ stockquote_interface/src/main/webapp/welcome.jsp
@@ -3,10 +3,10 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
-        <title>souhaogu_interface</title>
+        <title>stock_quote</title>
     </head>
     <body>
-        <h1>welcome to souhaogu.cn/stockquote_interface</h1>
+        <h1>welcome to souhaogu.cn/stock_quote</h1>
         <a href="${root}/stockBasic/getStockBasicsOfLikeStr.do?likeStr=fx">包含"fx"的股票信息列表</a><br>
         <a href="${root}/realtimeQuote/queryQuoteData.do?stockCode=SH600000&recentlyCount=30">浦发银行最近30分钟的实时行情</a><br>
         <a href="${root}/dayQuote/queryQuoteData.do?stockCode=SH600000&recentlyCount=30">浦发银行最近30天的历史行情</a><br>
