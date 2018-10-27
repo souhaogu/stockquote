15d26a2b3c9dec00747bfe0e1ab299b4f7c1c3aa
fly1232 <491876014@qq.com>
2018-10-27 22:32:58 +0800

    功能增加：     整理了异常处理     double保持小数点后2位

---
+++ stockquote_sync/src/main/java/com/soustock/stockquote/sync/common/ReturnMapHandler.java
@@ -0,0 +1,94 @@
+package com.soustock.stockquote.sync.common;
+
+
+import com.soustock.stockquote.exception.BusinessException;
+import com.soustock.stockquote.utils.JsonUtity;
+import com.soustock.stockquote.utils.StringUtity;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by xuyufei on 2018/10/27.
+ */
+public class ReturnMapHandler {
+
+    public static <T> T handleObject(String retMapStr, String resultKey, Class<T> resultClazz) throws BusinessException {
+        if (!StringUtity.isNullOrEmpty(retMapStr)) {
+            Map<String, Object> retMap = (Map<String, Object>) JsonUtity.readValue(retMapStr, Map.class);
+            if (retMap != null) {
+                boolean isSucc = (boolean) retMap.get("isSucc");
+                if (isSucc) {
+                    Object obj = retMap.get(resultKey);
+                    if (obj != null) {
+                        return (T) JsonUtity.readValue(obj.toString(), resultClazz);
+                    }
+                } else {
+                    String errorMsg = (String) retMap.get("errorMsg");
+                    if (!StringUtity.isNullOrEmpty(errorMsg)) {
+                        throw new BusinessException(errorMsg);
+                    }
+                }
+            }
+        }
+        throw new BusinessException("unknown error.");
+    }
+
+    public static <T> List<T> handleList(String retMapStr, String resultKey, Class<T> resultElementClazz) throws BusinessException {
+        if (!StringUtity.isNullOrEmpty(retMapStr)) {
+            Map<String, Object> retMap = (Map<String, Object>) JsonUtity.readValue(retMapStr, Map.class);
+            if (retMap != null) {
+                boolean isSucc = (boolean) retMap.get("isSucc");
+                if (isSucc) {
+                    Object obj = retMap.get(resultKey);
+                    if (obj != null) {
+                        return (List<T>) JsonUtity.readValueToList(obj.toString(), resultElementClazz);
+                    }
+                } else {
+                    String errorMsg = (String) retMap.get("errorMsg");
+                    if (!StringUtity.isNullOrEmpty(errorMsg)) {
+                        throw new BusinessException(errorMsg);
+                    }
+                }
+            }
+        }
+        throw new BusinessException("unknown error.");
+    }
+
+    public static void handleVoid(String retMapStr) throws BusinessException {
+        if (!StringUtity.isNullOrEmpty(retMapStr)) {
+            Map<String, Object> retMap = (Map<String, Object>) JsonUtity.readValue(retMapStr, Map.class);
+            if (retMap != null) {
+                boolean isSucc = (boolean) retMap.get("isSucc");
+                if (!isSucc) {
+                    String errorMsg = (String) retMap.get("errorMsg");
+                    if (!StringUtity.isNullOrEmpty(errorMsg)) {
+                        throw new BusinessException(errorMsg);
+                    }
+                }
+            }
+        }
+        throw new BusinessException("unknown error.");
+    }
+
+    public static <T> T handleType(String retMapStr, String resultKey) throws BusinessException {
+        if (!StringUtity.isNullOrEmpty(retMapStr)) {
+            Map<String, Object> retMap = (Map<String, Object>) JsonUtity.readValue(retMapStr, Map.class);
+            if (retMap != null) {
+                boolean isSucc = (boolean) retMap.get("isSucc");
+                if (isSucc) {
+                    Object obj = retMap.get(resultKey);
+                    if (obj != null) {
+                        return (T)obj;
+                    }
+                } else {
+                    String errorMsg = (String) retMap.get("errorMsg");
+                    if (!StringUtity.isNullOrEmpty(errorMsg)) {
+                        throw new BusinessException(errorMsg);
+                    }
+                }
+            }
+        }
+        throw new BusinessException("unknown error.");
+    }
+}
--- stockquote_sync/src/main/java/com/soustock/stockquote/sync/common/TargetDataCommon.java
+++ stockquote_sync/src/main/java/com/soustock/stockquote/sync/common/TargetDataCommon.java
@@ -1,6 +1,7 @@
 package com.soustock.stockquote.sync.common;
 
 import com.alibaba.fastjson.JSON;
+import com.soustock.stockquote.exception.BusinessException;
 import com.soustock.stockquote.po.FuquanFactorPo;
 import com.soustock.stockquote.po.StockBasicPo;
 import com.soustock.stockquote.po.StockQuotePo;
@@ -25,16 +26,11 @@ public class TargetDataCommon {
      * @return
      * @throws IOException
      */
-    public static Long getMaxUpdateTimeOfStockUpdate() throws IOException {
+    public static Long getMaxUpdateTimeOfStockUpdate() throws Exception {
         String urlString = BASE_URL_QUOTE + "/stockBasic/getMaxUpdatetimeOfStockBasic.do";
         HttpRequester httpRequester = new HttpRequester();
         String retStr = httpRequester.sendGet(urlString);
-        if (!StringUtity.isNullOrEmpty(retStr)){
-            Map<String, Object> retMap = JSON.parseObject(retStr, Map.class);
-            return (Long) retMap.get("result");
-        } else {
-            return null;
-        }
+        return ReturnMapHandler.handleType(retStr, "result");
     }
 
     /**
@@ -42,12 +38,13 @@ public class TargetDataCommon {
      * @return
      * @throws IOException
      */
-    public static void  insertOrUpdateStockBasics(List<StockBasicPo> stockBasicPos) throws IOException {
+    public static void insertOrUpdateStockBasics(List<StockBasicPo> stockBasicPos) throws Exception {
         String urlString = BASE_URL_QUOTE + "/stockBasic/insertOrUpdateStockBasics.do";
         HttpRequester httpRequester = new HttpRequester();
         Map<String, String> params = new HashMap<>();
-        params.put("list",  JsonUtity.writeValueAsString(stockBasicPos));
-        httpRequester.sendPost(urlString, params);
+        params.put("list", JsonUtity.writeValueAsString(stockBasicPos));
+        String retStr = httpRequester.sendPost(urlString, params);
+        ReturnMapHandler.handleVoid(retStr);
     }
 
 
@@ -56,18 +53,13 @@ public class TargetDataCommon {
      * @return
      * @throws IOException
      */
-    public static String getMaxDateOfStockQuote(String stockCode) throws IOException {
-        String urlString = BASE_URL_QUOTE + "/dayQuote/getMaxDateOfStock.do";
+    public static String getMaxDateOfStockQuote(String stockCode) throws Exception {
+        String urlString = BASE_URL_QUOTE + "/dayQuote/getMaxTradeDateOfStock.do";
         HttpRequester httpRequester = new HttpRequester();
         Map<String, String> params = new HashMap<>();
         params.put("stockCode", stockCode);
         String retStr = httpRequester.sendGet(urlString, params);
-        if (!StringUtity.isNullOrEmpty(retStr)){
-            Map<String, Object> retMap = JSON.parseObject(retStr, Map.class);
-            return (String) retMap.get("result");
-        } else {
-            return null;
-        }
+        return ReturnMapHandler.handleType(retStr, "result");
     }
 
     /**
@@ -75,12 +67,13 @@ public class TargetDataCommon {
      * @return
      * @throws IOException
      */
-    public static void  insertDayQuotes(List<StockQuotePo> dayQuotePoList) throws IOException {
+    public static void insertDayQuotes(List<StockQuotePo> dayQuotePoList) throws Exception {
         String urlString = BASE_URL_QUOTE + "/dayQuote/insertDayQuotes.do";
         HttpRequester httpRequester = new HttpRequester();
         Map<String, String> params = new HashMap<>();
         params.put("list", JsonUtity.writeValueAsString(dayQuotePoList));
-        httpRequester.sendPost(urlString, params);
+        String retStr = httpRequester.sendPost(urlString, params);
+        ReturnMapHandler.handleVoid(retStr);
     }
 
 
@@ -89,18 +82,13 @@ public class TargetDataCommon {
      * @return
      * @throws IOException
      */
-    public static String getMaxDateOfFuquanFactor(String stockCode) throws IOException {
+    public static String getMaxDateOfFuquanFactor(String stockCode) throws Exception {
         String urlString = BASE_URL_QUOTE + "/fuquanFactor/getMaxDateOfStockFuquan.do";
         HttpRequester httpRequester = new HttpRequester();
         Map<String, String> params = new HashMap<>();
         params.put("stockCode", stockCode);
         String retStr = httpRequester.sendGet(urlString, params);
-        if (!StringUtity.isNullOrEmpty(retStr)){
-            Map<String, Object> retMap = JSON.parseObject(retStr, Map.class);
-            return (String) retMap.get("result");
-        } else {
-            return null;
-        }
+        return ReturnMapHandler.handleType(retStr, "result");
     }
 
     /**
@@ -108,13 +96,12 @@ public class TargetDataCommon {
      * @return
      * @throws IOException
      */
-    public static void  insertFuquanFactors(List<FuquanFactorPo> fuquanFactorPoList) throws IOException {
+    public static void insertFuquanFactors(List<FuquanFactorPo> fuquanFactorPoList) throws Exception {
         String urlString = BASE_URL_QUOTE + "/fuquanFactor/insertFuquanFactors.do";
         HttpRequester httpRequester = new HttpRequester();
         Map<String, String> params = new HashMap<>();
         params.put("list", JsonUtity.writeValueAsString(fuquanFactorPoList));
-        httpRequester.sendPost(urlString, params);
+        String retStr = httpRequester.sendPost(urlString, params);
+        ReturnMapHandler.handleVoid(retStr);
     }
-
-
 }
